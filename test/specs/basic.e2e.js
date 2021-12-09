78f6207a1f597ebb899280b815df7d938fd558af
ffos-user-57 <fomazic@ffos.hr>
2021-12-09 19:23:16 +0100

    TESTOVIIII HEHEHE
    Prva dva testaaaaaaa

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,21 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
-    })
+describe('Open Shoutem Website - Pricing', () => {
+    it('Proper h1 is displayed', async () => {
+        browser.url('https://shoutem.com/pricing/');
+        const naslov = await $('h1');
+        await expect(naslov).toHaveText('There are more ways to create apps with Shoutem');
+        //just run with npm run-script wdio run wdio.conf.js which is in the main folder
+        //ofc npm install before it
+    
+    });
+   
+    it('Clicked the button and proper h1 is displayed', async () => {
+        browser.url('https://shoutem.com/apps/social-network/');
+        const elem = await $('a=Build now');
+        elem.click();
+        const chooseNaslov = await $("h1");
+        await expect(chooseNaslov).toHaveText('Choose an app template');
+        
+    
+    });
+
 })
